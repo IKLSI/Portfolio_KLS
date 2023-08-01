decc5e45bb6c202126e0c78642931bd399eb1851
KLS <kyliannlvs2@gmail.com>
2023-08-01 16:27:32 +0200

    Amélioration interface responsive

--- JS/menu.js
+++ JS/menu.js
@@ -1,6 +1,6 @@
 // Permet d'afficher le menu grâce à un bouton pour le responsive
 
-const menuToggle = document.getElementById('menu_Button');
+const menuToggle = document.getElementsByClassName('btn')[0];
 const menuList = document.querySelector('nav ul');
 
 menuToggle.addEventListener('click', () => {
