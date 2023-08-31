ae0b9a53e2911240aa60ea0539fe7ed9d615459c
KLS <100925067+IKLSI@users.noreply.github.com>
2023-08-31 17:34:21 +0200

    Suppression JS/menu.js

--- JS/menu.js
+++
@@ -1,8 +0,0 @@
-// Permet d'afficher le menu grâce à un bouton pour le responsive
-
-const menuToggle = document.querySelector('.btn');
-const menuList = document.querySelector('nav ul');
-
-menuToggle.addEventListener('click', () => {
-	menuList.classList.toggle('show');
-});
