62e4ee91d072981f7ed1e5bec19f905e82dbc331
KLS <you@example.com>
2024-01-03 14:15:02 +0100

    Modification contenu, style et blocage du scroll au chargement

--- JS/Loader.js
+++ JS/Loader.js
@@ -1,42 +1,64 @@
+var chargement = false;
+
 const load = gsap.timeline({
-	paused: "true"
-})
+	paused: true,
+});
+
 load.to("#percent , #bar", {
-	duration: .2,
+	duration: 0.2,
 	opacity: 0,
-	zIndex: -1
-})
-load.to(".progress",{
-	duration: .8,
-	width: "0%"
-})
+	zIndex: -1,
+});
+
+load.to(".progress", {
+	duration: 0.8,
+	width: "0%",
+});
+
 load.from(".content", {
-	duration: .8,
+	duration: 0.8,
 	opacity: 0,
-	ease: "Power4.out"
-}, "-=.5")
+	ease: "Power4.out",
+}, "-=.5");
+
 load.from(".content h1", {
-	duration: .5,
+	duration: 0.5,
 	y: 50,
 	skewY: 10,
-	opacity: 0
-},"-=1")
+	opacity: 0,
+}, "-=1");
 
 var id, width = 1;
-function loading(){
+
+function loading() {
 	id = setInterval(frame, 25);
 }
-function frame(){
-	if(width>=100){
-		clearInterval(id)
+
+function frame() {
+	if (width >= 100) {
+		clearInterval(id);
 		load.play();
-	}
-	else{
+		chargement = true;
+		enableScroll();
+	} else {
 		width++;
 		document.getElementById("barconfirm").style.width = width + '%';
 		document.getElementById("percent").innerHTML = width + '%';
 	}
 }
-window.onload = function(){
+
+window.onload = function () {
 	loading();
 }
+
+function disableScroll() {
+	document.body.style.overflow = 'hidden';
+}
+
+function enableScroll() {
+	document.body.style.overflow = '';
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+	disableScroll();
+});
